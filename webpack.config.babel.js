e1785cc7306c8348897cac110244fd13213a9384
Willmer Barahona <wbarahona@live.com>
2020-04-28 16:17:00 -0600

    Processing images, removed css and js references, cleaned build, hash
    Now it processes images, fonts, removed the css and js references from the template now is handled by webpack, cleaned the dist folder each compile, hashed the assets

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,16 +1,29 @@
 import * as path from 'path';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import HandlebarsPlugin from 'handlebars-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
+
+// Plugin instances and configs
+// -----------------------------------------------------------------------
 const extractPlugin = new ExtractTextPlugin({
-  filename: 'main.css'
+  filename: 'css/main.[hash].css'
 });
 
-const hbsPlugin= new HandlebarsPlugin({
+const hbsPlugin = new HandlebarsPlugin({
   entry: path.resolve(__dirname, 'src/templates/*.hbs'),
   output: path.resolve(__dirname, 'dist/[name].html')
 });
 
+const htmlPlugin = new HtmlWebpackPlugin({
+  template: 'src/templates/index.hbs'
+});
+
+const cleanPlugin = new CleanWebpackPlugin();
+
+// Work the bundling magic
+// -----------------------------------------------------------------------
 export default {
   context: path.resolve(__dirname),
   entry: './src/index.ts',
@@ -26,18 +39,51 @@ export default {
       },
       {
         test: /\.tsx?$/,
-        use: {
-          loader: 'ts-loader'
-        },
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ],
         exclude: /node_modules/,
       },
+      {
+        test: /\.(svg|gif|png|jpe?g)$/i,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'img'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'fonts'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.hbs$/,
+        exclude: /node_modules/,
+        use: [ 'html-loader']
+      }
     ],
   },
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ],
   },
   output: {
-    filename: 'bundle.js',
+    filename: 'js/bundle.[hash].js',
     publicPath: '/',
     path: path.resolve(__dirname, 'dist'),
   },
@@ -46,5 +92,5 @@ export default {
     compress: true,
     port: 8000,
   },
-  plugins: [ extractPlugin, hbsPlugin ]
+  plugins: [ extractPlugin, hbsPlugin, htmlPlugin, cleanPlugin ]
 }
